fb7fdc60580c9b6c3e3458699c964f3b9b1cba37
Sven <glaglasven@live.com>
2016-12-18 15:56:29 +0100

    Gaussian

--- SJCAM/MainPage.xaml.cs
+++ SJCAM/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Effects;
 using Microsoft.Graphics.Canvas.UI.Xaml;
+using SJCAM.Effects;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,11 +12,15 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
@@ -30,18 +35,15 @@ namespace SJCAM
     /// </summary>
     public sealed partial class MainPage : Page
     {
-		CanvasBitmap bitmapTiger;
-		ICanvasImage effect;
+		Color Dominant;
+		public string Title { get; set; }
+		public string Description { get; set; }
 
 		public MainPage()
         {
             this.InitializeComponent();
-			Init();
-        }
-
-		private async void Init()
-		{
-			
+			Title = "SJCAM";	
+			Description = "Welcome!";	
 		}
 
 		private void HamburgerButton_Click(object sender, RoutedEventArgs e)
@@ -49,40 +51,13 @@ namespace SJCAM
 			appSplit.IsPaneOpen = !appSplit.IsPaneOpen;
 		}
 
-		private void backgroundCanvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
+		private async void backgroundImage_Loaded(object sender, RoutedEventArgs e)
 		{
-			var size = sender.Size;
-			var ds = args.DrawingSession;
-
-			ds.DrawImage(effect, new Rect(0, 0, size.Width, size.Height), new Rect(0, 0, 1000, 625));
-			Debug.WriteLine("Draw");
-		}
-
-
-		private void backgroundCanvas_CreateResources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
-		{
-			Debug.WriteLine("CreateResource 1");
-			args.TrackAsyncAction(backgroundCanvas_CreateResourcesAsync(sender).AsAsyncAction());
-		}
-
-		private async Task backgroundCanvas_CreateResourcesAsync(CanvasControl sender)
-		{
-			Debug.WriteLine("CreateReources 2");
-			bitmapTiger = await CanvasBitmap.LoadAsync(sender, new Uri("http://d13fo19pohlmh8.cloudfront.net/media/assets/09e3fc49dbbcf9b40049c1e8ecf068ea.jpeg"));
-
-			effect = CreateGaussianBlur();
-		}
-
-		private ICanvasImage CreateGaussianBlur()
-		{
-			var blurEffect = new GaussianBlurEffect
-			{
-				Source = bitmapTiger,
-				BorderMode = EffectBorderMode.Hard,
-				BlurAmount = 3
-			};
-			Debug.WriteLine("Gaussian");
-			return blurEffect;
+			Dominant = await Images.saveImage(backgroundImage);
+			splitGrid.Background = new SolidColorBrush(Dominant);
+			var view = ApplicationView.GetForCurrentView();
+			view.TitleBar.BackgroundColor = Dominant;
+			view.TitleBar.ButtonBackgroundColor = Dominant;
 		}
 	}
 }

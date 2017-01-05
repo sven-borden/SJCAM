e00332ab8d8a4cda0b75ea43cc9e1aa85dce6eb0
Sven <glaglasven@live.com>
2017-01-05 14:49:46 +0100

    Glossy & sexy

---
+++ SJCAM/SJCAM/Logic/Settings/ToggleSetting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SJCAM.Logic.Settings
+{
+	class ToggleSetting : Setting
+	{
+		public bool IsOn { get; set; }
+
+		public ToggleSetting()
+		{
+			
+		}
+
+		public ToggleSetting(string _n)
+		{
+			this.Name = _n;
+		}
+	}
+}
--- SJCAM/SJCAM/MainPage.xaml.cs
+++ SJCAM/SJCAM/MainPage.xaml.cs
@@ -16,6 +16,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using SJCAM.Logic;
 using SJCAM.Style;
+using System.Collections.ObjectModel;
+using SJCAM.Logic.Settings;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace SJCAM
@@ -28,17 +30,27 @@ namespace SJCAM
 		public string Title = string.Empty;
 		public string Description = string.Empty;
 		public string ConnectionStatusText = string.Empty;
+		public Visibility connectionBarVisibility;
+		public ObservableCollection<object> ListSettings;
+
         public MainPage()
         {
+			ListSettings = new ObservableCollection<object>
+			{
+				new Setting(){Name = "test", Description = "descrip" },
+				new Setting(){Name = "test", Description = "2" }
+			};
 			Title = "SJCAM";
 			Description = "Choose a category";
 			ConnectionStatusText = "Try connecting";
+			connectionBarVisibility = Visibility.Collapsed;
             this.InitializeComponent();
 			CheckConnection();
         }
 
 		private async void CheckConnection()
 		{
+			connectionBarVisibility = Visibility.Visible;
 			bool _conn = await ConnectionStatus.WifiNameAsync(ConnectStatusProgressBar);
 			if (_conn)
 				ConnectionStatusText = "Connected";
@@ -48,6 +60,9 @@ namespace SJCAM
 				ConnectStatusProgressBar.IsIndeterminate = true;
 			}
 			ConnectStatusBar.Background = AppColor.GetConnectionColor(_conn);
+			DispatcherTimer coverOut = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 2) };
+			coverOut.Tick += (t, e) => { connectionBarVisibility = Visibility.Collapsed; (t as DispatcherTimer).Stop(); this.Bindings.Update(); }; 
+			coverOut.Start();
 		}
 
 		private async void Image_Loaded(object sender, RoutedEventArgs e)
